1079804e7f798a23f724f1880536dd03e3b0d03f
jelly-markbell <wanikoera24@gmail.com>
2015-08-30 14:58:48 +0900

    rail
    レール作った

---
+++ toroko/Assets/script/toroko_play.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+
+function Start () {
+		GetComponent.<Rigidbody>().AddForce(2000,0,0);
+
+}
+
+function Update () {
+
+		if ( GetComponent.<Transform>().position.x == 100 ){
+		GetComponent.<Rigidbody>().position = Vector3.zero;
+		
+	}	
+
+}
