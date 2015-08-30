59ea720d646aa94408ecb69652160e5bef206c5a
jelly-markbell <wanikoera24@gmail.com>
2015-08-30 16:02:45 +0900

    トロッコのスクリプト半分で来たノン
    トロッコがぁあああああああああああああああああああああああああああ！！！
    動いてる！！！！！！

--- toroko/Assets/script/toroko_play.js
+++ toroko/Assets/script/toroko_play.js
@@ -8,8 +8,8 @@ function Start () {
 
 function Update () {
 
-		if ( GetComponent.<Transform>().position.x == 100 ){
-		GetComponent.<Rigidbody>().position = Vector3.zero;
+		if ( GetComponent.<Transform>().position.x > 200 ){
+		GetComponent.<Rigidbody>().position = Vector3(-25,4.5,0);
 		
 	}	
 
